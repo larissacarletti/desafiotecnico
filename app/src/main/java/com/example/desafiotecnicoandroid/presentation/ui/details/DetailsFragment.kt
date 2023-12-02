48c36496c5491eca034d9d695e3ce9349ef1b0d4
Larissa Leal Carletti <122300740+larissacarletti@users.noreply.github.com>
2023-12-02 11:59:37 -0300

    date formatting and navigation

--- app/src/main/java/com/example/desafiotecnicoandroid/presentation/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/desafiotecnicoandroid/presentation/ui/details/DetailsFragment.kt
@@ -1,42 +1,59 @@
 package com.example.desafiotecnicoandroid.presentation.ui.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.desafiotecnicoandroid.R
 import com.example.desafiotecnicoandroid.data.models.FlightsItem
 import com.example.desafiotecnicoandroid.databinding.FragmentDetailsBinding
+import com.example.desafiotecnicoandroid.utils.formatDate
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
-    private lateinit var binding : FragmentDetailsBinding
+    private lateinit var binding: FragmentDetailsBinding
+    private val args: DetailsFragmentArgs by navArgs()
+    private lateinit var flight: FlightsItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setFlightInformation()
+        setUpToolbar()
 
     }
 
-    private fun setFlightInformation(flight: FlightsItem) = binding.run {
-        carrierDetails.text = flight.carrier
+    private fun setFlightInformation() = binding.run {
+        flight = args.flight
+        carrierDetails.text = flight.carrier.capitalize()
         detailsDeparture.text = getString(
             R.string.iata_departure,
             flight.departure.locationCode,
-            flight.departure.city)
+            flight.departure.city
+        )
         detailsArrival.text = getString(
             R.string.iata_arrival,
             flight.arrival.locationCode,
-            flight.arrival.city)
-
+            flight.arrival.city
+        )
+        timeDepartureDetails.text = formatDate(flight.departure.dateTime)
+        timeArrivalDetails.text = formatDate(flight.arrival.dateTime)
     }
 
+    private fun setUpToolbar() = binding.run {
+        toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
--- app/src/main/java/com/example/desafiotecnicoandroid/utils/DateExtensions.kt
+++ app/src/main/java/com/example/desafiotecnicoandroid/utils/DateExtensions.kt
@@ -7,10 +7,10 @@ import java.util.TimeZone
 
 fun formatDate(stringDate: String): String {
     val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-    inputFormat.timeZone = TimeZone.getTimeZone("UTC")
+    inputFormat.timeZone = TimeZone.getTimeZone("America/Sao_Paulo")
     val date: Date = inputFormat.parse(stringDate)
 
-    val outputFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+    val outputFormat = SimpleDateFormat("HH:mm", Locale.US)
     outputFormat.timeZone = TimeZone.getTimeZone("America/Sao_Paulo")
     return outputFormat.format(date)
 }
